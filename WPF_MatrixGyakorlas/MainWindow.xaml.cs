0e5c6f94c3c3b912db873b0aa1eebbc3e9d9e60d
Petrik Diak <diak@petrik.hu>
2026-03-04 09:00:24 +0100

    Gombok alaphelyzete.

--- WPF_MatrixGyakorlas/MainWindow.xaml.cs
+++ WPF_MatrixGyakorlas/MainWindow.xaml.cs
@@ -14,9 +14,40 @@ namespace WPF_MatrixGyakorlas
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
-        public MainWindow()
+		private void console_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+
+		private void btnClose_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void btnSize_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void btnSleep_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void btnCreate_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnSave_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+		
+		public MainWindow()
         {
             InitializeComponent();
         }
